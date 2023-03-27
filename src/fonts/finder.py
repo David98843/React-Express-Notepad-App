9b75a4e29f49546697aea8da27861a3aa59728b0
David Akinloye <69762085+David98843@users.noreply.github.com>
2023-03-27 07:06:12 +0100

    Delete src directory

--- src/fonts/finder.py
+++
@@ -1,6 +0,0 @@
-from sklearn.linear_model import Perceptron, SGDClassifier
-import inspect
-from sklearn.model_selection import train_test_split
-
-model = Perceptron()
-print(inspect.getsource(SGDClassifier.fit))
